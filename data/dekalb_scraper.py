e97bf917e39d93a8325d7dde80e0258bfd900964
abrie <abrie@users.noreply.github.com>
2022-01-15 17:40:11 -0500

    First commit with skeletal scraper logic. Scrapes cases for all judicial officers within hardcoded time range.

--- data/dekalb_scraper.py
+++ data/dekalb_scraper.py
@@ -3,25 +3,80 @@ import json
 from bs4 import BeautifulSoup
 
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
-}
+def get_available_criteria(session, headers):
+    url = "https://ody.dekalbcountyga.gov/portal/Home/Dashboard/26"
+    response = session.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-url = "https://ody.dekalbcountyga.gov/portal/Home/Dashboard/26"
-session = requests.Session()
-response = session.get(url, headers=headers)
-soup = BeautifulSoup(response.content, "html.parser")
+    return {
+        "judicial_officers": [
+            {"value": el.get("value"), "name": el.get_text()}
+            for el in soup.select("#selHSJudicialOfficer option")
+        ],
+        "courtrooms": [
+            {"value": el.get("value"), "name": el.get_text()}
+            for el in soup.select("#selHSCourtroom option")
+        ],
+    }
 
-judicial_officers = [
-    {"value": el.get("value"), "name": el.get_text()}
-    for el in soup.select("#selHSJudicialOfficer option")
-]
 
-courtrooms = [
-    {"value": el.get("value"), "name": el.get_text()}
-    for el in soup.select("#selHSCourtroom option")
-]
+def submit_search_by_judicial_officer(
+    session, headers, judicial_officer, date_from, date_to
+):
+    payload = {
+        "PortletName": "HearingSearch",
+        "Settings.CaptchaEnabled": "False",
+        "Settings.DefaultLocation": "All Courts",
+        "SearchCriteria.SelectedCourt": "All Courts",
+        "SearchCriteria.SelectedHearingType": "All Hearings",
+        "SearchCriteria.SearchByType": "JudicialOfficer",
+        "SearchCriteria.SelectedJudicialOfficer": "284",
+        "SearchCriteria.DateFrom": "01/31/2022",
+        "SearchCriteria.DateTo": "02/24/2022",
+    }
+    search_result = session.post(
+        "https://ody.dekalbcountyga.gov/portal/Hearing/SearchHearings/HearingSearch",
+        data=payload,
+        headers=headers,
+    )
 
-result = {"judicial_officers": judicial_officers, "courtrooms": courtrooms}
 
-print(json.dumps(result))
+def read_search_result(session, headers):
+    payload = {
+        "sort": "",
+        "group": "",
+        "filter": "",
+        "portletId": "27",
+    }
+
+    result = session.post(
+        "https://ody.dekalbcountyga.gov/portal/Hearing/HearingResults/Read",
+        data=payload,
+        headers=headers,
+    )
+
+    return result.content
+
+
+def run():
+    headers = {"User-Agent": "CodeForAtlanta Court Bot"}
+
+    session = requests.Session()
+
+    criteria = get_available_criteria(session, headers)
+
+    results = []
+
+    for criterium in criteria["judicial_officers"]:
+        judicial_officer = criterium["name"]
+        submit_search_by_judicial_officer(
+            session, headers, criterium["value"], "01/31/2022", "02/24/2022"
+        )
+
+        result = read_search_result(session, headers)
+        results.append(json.loads(result))
+
+    print(json.dumps(results))
+
+
+run()
