418df79f8790db6f27990520979428a41df962c2
abrie <abrie@users.noreply.github.com>
2022-01-14 23:51:39 -0500

    Scrape courtrooms and judicial officers names and ids.

--- data/dekalb_scraper.py
+++ data/dekalb_scraper.py
@@ -1,1 +1,27 @@
-print('Hello Dekalb')
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
+}
+
+url = "https://ody.dekalbcountyga.gov/portal/Home/Dashboard/26"
+session = requests.Session()
+response = session.get(url, headers=headers)
+soup = BeautifulSoup(response.content, "html.parser")
+
+judicial_officers = [
+    {"value": el.get("value"), "name": el.get_text()}
+    for el in soup.select("#selHSJudicialOfficer option")
+]
+
+courtrooms = [
+    {"value": el.get("value"), "name": el.get_text()}
+    for el in soup.select("#selHSCourtroom option")
+]
+
+result = {"judicial_officers": judicial_officers, "courtrooms": courtrooms}
+
+print(json.dumps(result))
