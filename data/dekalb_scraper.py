794b33176afc9a6c13923530cd82bed8636bcaa6
abrie <abrie@users.noreply.github.com>
2022-01-16 11:34:41 -0500

    Refactor.

--- data/dekalb_scraper.py
+++ data/dekalb_scraper.py
@@ -10,17 +10,17 @@ class API:
         self.headers = {"User-Agent": "CodeForAtlanta Court Bot"}
         self.session = requests.Session()
 
-    def get_available_criteria(self):
+    def get_all_judicial_officers(self):
         url = "https://ody.dekalbcountyga.gov/portal/Home/Dashboard/26"
         response = self.session.get(url, headers=self.headers)
         soup = BeautifulSoup(response.content, "html.parser")
 
-        return {
-            "judicial_officers": [
-                {"id": el.get("value"), "name": el.get_text()}
-                for el in soup.select("#selHSJudicialOfficer option")
-            ],
-        }
+        result = [
+            {"id": el.get("value"), "name": el.get_text()}
+            for el in soup.select("#selHSJudicialOfficer option")
+        ]
+
+        return filter(lambda e: e["name"] != "", result)
 
     def submit_search_by_judicial_officer(self, name, date_from, date_to):
         to_date_string = lambda d: datetime.date.strftime(d, "%m/%d/%Y")
@@ -63,23 +63,28 @@ class API:
 
         return json.loads(response.content)
 
-    def fields_of_interest(self, case):
-        return {
-            "CaseId": case["CaseId"],
-            "HearingDate": case["HearingDate"],
-            "HearingTime": case["HearingTime"],
-            "CourtRoom": case["CourtRoom"],
-        }
+    def get_cases_by_judicial_officer(self, officer, date_from, date_to):
+        log(f'{officer["id"]}\t{officer["name"]}')
+        self.submit_search_by_judicial_officer(officer["id"], date_from, date_to)
+
+        return self.get_search_result()["Data"]
 
 
 def log(str):
     print(str, file=sys.stderr)
 
 
+def take_fields_of_interest(case):
+    return {
+        "CaseId": case["CaseId"],
+        "HearingDate": case["HearingDate"],
+        "HearingTime": case["HearingTime"],
+        "CourtRoom": case["CourtRoom"],
+    }
+
+
 def run():
     api = API()
-
-    criteria = api.get_available_criteria()
 
     date_from = datetime.date.today()
     date_to = date_from + datetime.timedelta(days=90)
@@ -89,15 +94,10 @@ def run():
     log("Scraping Dekalb County court cases per judicial officer...")
     log("ID\tName")
 
-    for judicial_officer in criteria["judicial_officers"]:
-        log(f'{judicial_officer["id"]}\t{judicial_officer["name"]}')
-        api.submit_search_by_judicial_officer(
-            judicial_officer["id"], date_from, date_to
-        )
+    for officer in api.get_all_judicial_officers():
+        cases = api.get_cases_by_judicial_officer(officer, date_from, date_to)
+        results.extend([take_fields_of_interest(case) for case in cases])
 
-        result = api.get_search_result()
-        result = [api.fields_of_interest(case) for case in result["Data"]]
-        results.extend(result)
     log("Finished.")
 
     print(json.dumps(results))
