03226154afbd1df7ef948ec51bd23d6b78eed2e1
Andrew Trivette <andrew.trivette@gmail.com>
2022-02-02 04:49:15 -0500

    Update twilio_functions.js

--- twilio/twilio_functions.js
+++ twilio/twilio_functions.js
@@ -31,65 +31,17 @@ exports.handler = async function (context, event, callback) {
         str.match(/(\d{2})([A-Z]{1,2})(\d{4,5})/) ||
         str.match(/([A-Z]{1})(\d{7})/);
 
-      console.log(regex_res);
-
-      if (!regex_res) {
-        return callback(null, {
-          case_found: false,
-          error_msg: "That does not look like a valid case number.",
-        });
-      }
-
-      // The regex match function returns an object.
-      // We only want the matching string, which is the first item in the array.
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
-      const case_number = regex_res[0];
-
-      // Check if this case number exists in the db
-      const sqlQuery = `
-        SELECT 1
-        FROM \`georgia-courtbot.GA_DEKALB_LANDING.daily_scraped_hearings\`
-        WHERE CaseNumber='${case_number}'`;
-      console.log(sqlQuery);
-
-      const options = {
-        query: sqlQuery,
-        // Location must match that of the dataset(s) referenced in the query.
-        location: "us-east1",
-      };
-
-      const [rows] = await bigqueryClient.query(options);
-      console.log("BigQuery result:");
-      console.log(rows);
-
-      if (rows.length) {
-        return callback(null, {
-          case_found: true,
-          case_number: case_number,
-        });
-      } else {
-        return callback(null, {
-          case_found: false,
-          error_msg: `Sorry, we have no record of case number '${case_number}'.`,
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      return callback(error);
-    }
-  } else if (event.case_number) {
-    try {
       const sqlQuery = `
         SELECT *
-        FROM \`georgia-courtbot.GA_DEKALB_LANDING.daily_scraped_hearings\`
-        WHERE CaseNumber='${event.case_number.trim().toUpperCase()}'
+        FROM \`cfa-georgia-courtbot.STG.HEARING\`
+        WHERE CaseNumber='${regex_res[0]}'
         LIMIT 1`;
       console.log(sqlQuery);
 
       const options = {
         query: sqlQuery,
         // Location must match that of the dataset(s) referenced in the query.
-        location: "us-east1",
+        location: "us",
       };
 
       const [rows] = await bigqueryClient.query(options);
@@ -104,7 +56,7 @@ exports.handler = async function (context, event, callback) {
           courtroom: rows[0].CourtRoom,
         });
       } else {
-        return callback(null, { hearing_found: false });
+        return callback({ hearing_found: false });
       }
     } catch (error) {
       console.log(error);
